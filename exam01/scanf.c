73b8b117a0142157ced98d1a81c0943993ea1ca4
sang kyu choi <wibro77@gmail.com>
2018-03-05 16:51:09 +0900

    산술논리 연산(정육면체 부피구하기)

--- exam01/scanf.c
+++ exam01/scanf.c
@@ -10,10 +10,11 @@ void main() {
 }
 */
 
+/* scanf_s() ,gets()등 입,출력함수 
 int main(void) {
 	int age;
 	double height, weight;
-	char bloodtype[5];
+	char bloodtype[3];
 	char name[10];
 
 
@@ -26,4 +27,19 @@ int main(void) {
 	printf("이름은: %s 나이: %d 키: %.1lf 몸무게:%.1lf 혈액형:%s 입니다.\n\n",name, age, height, weight, bloodtype);
 
 	return 0;
+}
+*/
+
+/*산술논리 연산(정육면체 부피) */
+
+void main() {
+
+	int row, col, height;
+	double volume =0.0;
+
+	printf("가로,세로,높이를 순서대로 입력하세요.\n");
+	scanf_s("%d %d %d", &row, &col, &height);
+	volume = row * col * height;
+	printf("\n정육면체 부피: %.1lf", volume);
+
 }
