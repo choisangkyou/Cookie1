dc0377ad2b6ea681cb4faa4e854aa5317a1c7de9
sang kyu choi <wibro77@gmail.com>
2018-03-07 15:26:17 +0900

    조건문 if

--- exam01/condition.c
+++ exam01/condition.c
@@ -1,13 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS //scanf함수 에러 없이 사용하기 위해.
 #include <stdio.h>
 
-/*제어문 (조건,반복,분기)*/
+/*제어문 (조건문 )
 void main() {
 	int age;
 	printf("숫자를 입력하세요.");
 	scanf_s("%d", &age);
 
-	/* 홀수/짝수..
+	// 홀수/짝수..
 	if (age% 2 ==0) {
 		printf("짝수 입니다.");
 	}
@@ -15,10 +15,10 @@ void main() {
 
 		printf("홀수 입니다.");
 	}
-	*/
-
+	
+	printf("\n\r");
 	//0보다크면 양수 , 작으면 음수 같으면 0입니다.
-	/*
+	
 	if (age > 0) {
 		printf("양수 입니다.");
 	}
@@ -28,8 +28,6 @@ void main() {
 	else {
 		printf("0입니다.");
 	}
-	*/
-
-	//
-
 }
+*/
+
