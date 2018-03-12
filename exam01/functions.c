a635200251b8ec38442f191ca5666ec00c5f28c8
sang kyu choi <wibro77@gmail.com>
2018-03-12 17:00:22 +0900

    키보드 입력을 이용한 산술 연산 예제.

--- exam01/functions.c
+++ exam01/functions.c
@@ -6,24 +6,51 @@
 
 //매크로 선언 함수 상수 선언
 #define UP 72
-#define DOWn 80
+#define DOWN 80
+#define ENTER 13
+#define ESC 27
 
 void menu(int m);
+void writer();
 /*
-int add();
-int sub();
-int mul();
-int div();
+void add();
+void sub();
+void mul();
+void div();
 */
 int main(void ){
 
-	int n;
+	int key=1,navi;
 	//printf("[1]:더하기[2]빼기,[3]곱하기,[4]나누기,[5]종료하기 메뉴를 선택하세요:");
-	//scanf_s("%d", &n);
 	
+	menu(key);//기본 key(1)
+
 	while (1) {
-		menu(1);
-		n = _getch();//up, down 아스크값
+		navi = (int)_getch();//사용자 키조작 up(72), down(80) , Enter(13), Esc(27)
+		
+		switch (navi) {
+		case UP:
+			if(key !=1)key--;
+			menu(key);
+			break;
+		case DOWN:
+			 if(key !=6) key++;
+			 menu(key);
+			 break;
+		case ENTER:
+			
+			if (key == 5)
+				writer();
+			break;
+		case ESC:
+			exit(0);
+			break;
+			
+		}
+
+		//printf("key값:%d", navi);
+		
+		
 		//UP,DOWN 구분해서 메뉴선택하기..
 	}
 	return 0;
@@ -44,7 +71,8 @@ void menu(int m) {
 		printf("│                         2. 빼    기 ( - )                  │\n");
 		printf("│                         3. 곱 하 기 ( * )                  │\n");
 		printf("│                         4. 나 누 기 ( / )                  │\n");
-		printf("│                         5. 종료하기 ( ESC )                │\n");
+		printf("│                         5. 만 든 이                        │\n");
+		printf("│                         6. 종료하기 ( ESC )                │\n");
 		printf("│                                                            │\n");
 		printf("│                                                            │\n");
 		printf("┖━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙\n");
@@ -60,7 +88,8 @@ void menu(int m) {
 		printf("│                     ☞  2. 빼    기 ( - )                  │\n");
 		printf("│                         3. 곱 하 기 ( * )                  │\n");
 		printf("│                         4. 나 누 기 ( / )                  │\n");
-		printf("│                         5. 종료하기 ( ESC )                │\n");
+		printf("│                         5. 만 든 이                        │\n");
+		printf("│                         6. 종료하기 ( ESC )                │\n");
 		printf("│                                                            │\n");
 		printf("│                                                            │\n");
 		printf("┖━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙\n");
@@ -76,7 +105,8 @@ void menu(int m) {
 		printf("│                         2. 빼    기 ( - )                  │\n");
 		printf("│                     ☞  3. 곱 하 기 ( * )                  │\n");
 		printf("│                         4. 나 누 기 ( / )                  │\n");
-		printf("│                         5. 종료하기 ( ESC )                │\n");
+		printf("│                         5. 만 든 이                        │\n");
+		printf("│                         6. 종료하기 ( ESC )                │\n");
 		printf("│                                                            │\n");
 		printf("│                                                            │\n");
 		printf("┖━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙\n");
@@ -91,8 +121,9 @@ void menu(int m) {
 		printf("│                         1. 더 하 기 ( + )                  │\n");
 		printf("│                         2. 빼    기 ( - )                  │\n");
 		printf("│                         3. 곱 하 기 ( * )                  │\n");
-		printf("│                    ☞   4. 나 누 기 ( / )                  │\n");
-		printf("│                         5. 종료하기 ( ESC )                │\n");
+		printf("│                     ☞  4. 나 누 기 ( / )                  │\n");
+		printf("│                         5. 만 든 이                        │\n");
+		printf("│                         6. 종료하기 ( ESC )                │\n");
 		printf("│                                                            │\n");
 		printf("│                                                            │\n");
 		printf("┖━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙\n");
@@ -108,11 +139,42 @@ void menu(int m) {
 		printf("│                         2. 빼    기 ( - )                  │\n");
 		printf("│                         3. 곱 하 기 ( * )                  │\n");
 		printf("│                         4. 나 누 기 ( / )                  │\n");
-		printf("│                 ☞      5. 종료하기 ( ESC )                │\n");
+		printf("│                     ☞  5. 만 든 이                        │\n");
+		printf("│                         6. 종료하기 ( ESC )                │\n");
+		printf("│                                                            │\n");
+		printf("│                                                            │\n");
+		printf("┖━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙\n");
+		break;
+	case 6:
+		printf("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┒\n");
+		printf("│                                                            │\n");
+		printf("│                      ※사칙연산 프로그램                   │\n");
+		printf("│                                                            │\n");
+		printf("│                                                            │\n");
+		printf("│                                                            │\n");
+		printf("│                         1. 더 하 기 ( + )                  │\n");
+		printf("│                         2. 빼    기 ( - )                  │\n");
+		printf("│                         3. 곱 하 기 ( * )                  │\n");
+		printf("│                         4. 나 누 기 ( / )                  │\n");
+		printf("│                         5. 만 든 이                        │\n");
+		printf("│                     ☞  6. 종료하기 ( ESC )                │\n");
 		printf("│                                                            │\n");
 		printf("│                                                            │\n");
 		printf("┖━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙\n");
 		break;
 	
 	}
+
+
+}
+
+//만든이
+void writer() {
+	char wstr[] = "만든이 최상규 입니다.";
+
+	for (int i = 0; i < sizeof(wstr); i++) {
+		printf("%c",wstr[i]);
+		Sleep(100);
+	}
+	
 }
